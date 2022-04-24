37cc96c455f7d2f585da5c6e904359a245c55081
witekmikolajczak <witekmikolajczak@gmail.com>
2022-04-24 14:59:00 +0200

    p

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+
 // STYLES
 import styles from "../styles/Product.module.scss"
 export const Product = () => {
   return (
-    <div className={styles.product}>Product</div>
+    <div className={styles.product}>
+      <div className={styles['image-container']}>
+      </div>
+    </div>
   )
 }
